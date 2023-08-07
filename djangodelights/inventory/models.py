6a3b951e1edea5bba0e42ae569bf0c5edf2b6452
panagiota-charalampidou <giotacharalampidou@gmail.com>
2023-08-07 17:11:18 +0200

    initial models

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model):
+    ingredient = models.CharField()
+    available_quantity = models.FloatField()
+    measurement_unit = models.CharField()
+    price_per_unit = models.FloatField()
+
+class MenuItem(models.Model):
+    menu_item = models.CharField()
+    price = models.FloatField()
+
+class RecipeRequirements(models.Model):
+    menu_item = models.CharField()
+    ingredient = models.CharField()
+    ingredient_quantity = models.FloatField()
+    measurement_unit = models.CharField()
