07a529aa9e6d622f6463cc7f094f236cac74e9f2
panagiota-charalampidou <giotacharalampidou@gmail.com>
2023-08-09 09:16:19 +0200

    models first version, admin setup

--- djangodelights/inventory/admin.py
+++ djangodelights/inventory/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Ingredient, RecipeRequirements, MenuItem, Purchase
 
 # Register your models here.
+admin.site.register([Ingredient, RecipeRequirements, MenuItem, Purchase])
--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -1,18 +1,47 @@
 from django.db import models
+import datetime
 
 # Create your models here.
 class Ingredient(models.Model):
-    ingredient = models.CharField()
+    ingredient = models.CharField(max_length=40)
     available_quantity = models.FloatField()
-    measurement_unit = models.CharField()
+    measurement_unit = models.CharField(max_length=40)
     price_per_unit = models.FloatField()
 
-class MenuItem(models.Model):
-    menu_item = models.CharField()
-    price = models.FloatField()
+    class Meta:
+        ordering = ["ingredient"]
+    
+    def __str__(self):
+        return self.ingredient
 
 class RecipeRequirements(models.Model):
-    menu_item = models.CharField()
-    ingredient = models.CharField()
+    menu_item = models.CharField(max_length=40)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
     ingredient_quantity = models.FloatField()
-    measurement_unit = models.CharField()
+
+    class Meta:
+        ordering = ["menu_item"]
+
+    def __str__(self):
+        return self.menu_item
+
+class MenuItem(models.Model):
+    menu_item = models.ForeignKey(RecipeRequirements, on_delete=models.CASCADE)
+    price = models.FloatField()
+
+    class Meta:
+        ordering = ["menu_item"]
+
+    def __str__(self):
+        return self.menu_item
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    timestamp = models.DateField(default = datetime.date.today)
+
+    class Meta:
+        ordering = ["timestamp"]
+
+    def __str__(self):
+        return self.menu_item
+     
